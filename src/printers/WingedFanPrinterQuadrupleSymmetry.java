eb355e700bfa84b6e603506a93e9604df80e5506
Michallam <michal.gorski1997@gmail.com>
2018-05-05 14:34:35 +0200

    removed unnecessary comments

--- src/printers/WingedFanPrinterQuadrupleSymmetry.java
+++ src/printers/WingedFanPrinterQuadrupleSymmetry.java
@@ -11,18 +11,11 @@ public class WingedFanPrinterQuadrupleSymmetry implements WingedFanPrinter {
 
     @Override
     public void printFan(int dimension, FanDirection fanDirection){
-        long duration;
-        long startTime, endTime;
-
-        startTime = System.nanoTime();
         if(fanDirection == FanDirection.Clockwise){
             charMap.buildClockwiseFan(dimension);
         }else if (fanDirection == FanDirection.CounterClockwise){
             charMap.buildCunterClockwiseFan(dimension);
         }
-        /*endTime = System.nanoTime();
-        duration = endTime-startTime;
-        System.out.println("Duratrion of generating "+dimension+" fan: "+duration/1000000000.0+"s");*/
 
         charMap.print();
     }
